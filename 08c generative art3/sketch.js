e277d965701a74ac6c934f5701c7440c68e5c6aa
Silvercatmist <160178198+Silvercatmist@users.noreply.github.com>
2024-03-25 15:23:12 -0600

    Update sketch.js

--- 08c generative art3/sketch.js
+++ 08c generative art3/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// generative art 
+// Kaveen
+// march 25 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -8,8 +8,34 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noLoop();
+  background(220);
+  angleMode(DEGREES);
+  stroke(0,80)
 }
 
+function randonElement(currentLen){
+  //create one ragular line
+  // rotate transformation
+  push();
+  rotate(random(360));
+  while(currentLen> 5 ){
+    rotate(random(-40,40));
+    line(0,0,0,currentLen);
+    translate(0,currentLen);
+    currentLen *= 0.75;
+  }
+ pop();
+  
+}
+
+
+
 function draw() {
-  background(220);
+  translate(width/2,height/2);
+  for(let i = 0; i< 600; i++){
+    randonElement(random(50,80));
+  }
+  
+  
 }
