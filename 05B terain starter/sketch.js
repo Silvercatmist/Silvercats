045cab21af67ae09eacf7f39f3e8b07d3c7902af
Silvercatmist <160178198+Silvercatmist@users.noreply.github.com>
2024-03-26 15:16:55 -0600

    terrain generation
    generating a terrain using code.

---
+++ 05B terain starter/sketch.js
@@ -0,0 +1,73 @@
+// Terrain Generation starter
+// Kaveen
+// March 11 2024
+// precedurally generated 2d terrain
+
+let rectWidth = 20;
+let rectTime = 10;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  rectMode(CENTER);
+  drawRectangles();
+}
+
+function drawRectangles(){
+  // using a single loop, generate a bunch of side-to=side
+  //
+  let rectHeight;
+  fill(0);
+  for(let x = 0; x< width; x += rectWidth){
+    //option 1 - pattern
+    // rectHeight = x;
+
+    //option2 - random
+    rectHeight = random(0, height*0.8);
+
+    //option 3 perlin noise
+    rectHeight = noise(rectTime);
+    rectHeight = map(rectHeight,0,1,0, height);
+    rectTime += 0.1; 
+
+
+
+    rect(x, height/2,rectWidth, rectHeight);
+    
+  }
+}
+
+function widthChange(){
+  // using the arrow keys to change the width of the terrain
+  if(keyIsPressed){
+    
+    if(keyCode === LEFT_ARROW){
+      rectTime -= 10;
+      drawRectangles();
+    
+    }
+    else if(keyCode === RIGHT_ARROW){
+      rectTime += 10;
+      drawRectangles();
+    }
+
+    
+
+  }
+}
+
+
+function drawFlag(x,y){
+  let rectangleHeight = 20;
+  let rectangleWidth = 5;
+  let triHeight = 20;
+  rect(45,60,rectangleWidth,rectangleHeight);
+  triangle(55,60,55,65,65,63);
+
+
+}
+
+function draw() {
+  widthChange();
+  drawFlag(88,50);
+}
