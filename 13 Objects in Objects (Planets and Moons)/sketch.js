9ef7ea5be5d7ae608c228f4ebe32c6e3cea369b1
Silvercatmist <160178198+Silvercatmist@users.noreply.github.com>
2024-04-12 15:01:09 -0600

    planets and moons

---
+++ 13 Objects in Objects (Planets and Moons)/sketch.js
@@ -0,0 +1,85 @@
+// Objects in objects
+// Kaveen
+// April 12 2024
+// Storing objects i objects, overwriting objects, basic transformation
+
+let myPlanet;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  myPlanet = new Planet(width/2,height/2);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(50);
+  myPlanet.display();
+}
+
+function mouseClicked(){
+  if(keyIsPressed && keyCode === SHIFT){
+    myPlanet = new Planet(mouseX, mouseY);
+  }
+  else{
+    myPlanet.createMoon();
+  }
+}
+
+function keyPressed(){
+  if(keyCode!==SHIFT){
+    myPlanet.relocate(mouseX,mouseY);
+  }
+}
+
+class Planet{
+  constructor(x,y){
+    this.x = x; this.y = y; this.s = 100;
+    this.moons = [];
+  }
+
+  relocate(){
+    myPlanet.x = mouseX;
+    myPlanet.y = mouseY;
+    for(let m of this.moons){
+      m.x = mouseX;
+      m.y = mouseY;
+    }
+  }
+
+  createMoon(){
+    this.moons.push(new Moon(this.x,this.y));
+  }
+
+  display(){
+    circle(this.x,this.y,this.s);
+    for(let m of this.moons){
+      m.update();
+    }
+
+  }
+}
+
+
+class Moon{
+  constructor(x,y){
+    this.x = x; this.y = y; this.speed = 2;
+    this.angle = 0; this.orbitRadius = 80; this.s = 25;
+  }
+
+  update(){
+    this.move();
+    this.display();
+  }
+
+  move(){
+    this.angle += this.speed;
+  }
+  
+  display(){
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    circle(this.orbitRadius,0,this.s);
+    pop();
+  }
+}
