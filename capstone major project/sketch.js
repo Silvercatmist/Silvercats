700251c408187916635de1c9ac39ca22610ab57a
Silvercatmist <160178198+Silvercatmist@users.noreply.github.com>
2024-05-24 15:25:57 -0600

    Update sketch.js

--- capstone major project/sketch.js
+++ capstone major project/sketch.js
@@ -7,54 +7,96 @@
 let card;
 let row;
 let col;
+// let front = color("blue");
+// let back = color("red");
 
-let cardGrid = 
-[[0,0,0]
-[0,0,0]
-[0,0,0]];
+let cardGrid =
+  [[]];
 
-let squareSize = 10;
+
 const NUM_ROWS = 3; const NUM_COLS = 3;
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  card = new Cards(width / 2, height / 2, 0);
 }
 
 function draw() {
   background(220);
   drawScreen();
+  card.display();
+  card.move();
 }
 
+// function mouseClicked() {
+ 
+// }
 
-function drawScreen(){
+
+function drawScreen() {
   // creating the screen background
   rectMode(CENTER);
-  fill(204,229,255);
-  rect(width/2,height/2,width/2 + 380, height/2 + 120)
+  fill(204, 229, 255);
+  rect(width / 2, height / 2, width / 2 + 380, height / 2 + 120)
 }
 
-function getCurrentX(){}
-function getCurrentY(){}
+// function getCurrentX(){
+//   let constrainX = constrain(mouseX, 0, width - 1);
+//   return int(constrainX / squareSize);
+// }
 
-function drawCardGrid(){}
+// function getCurrentY(){
+//   let constrainY = constrain(mouseY, 0, height- 1);
+//   return int(constrainY / squareSize);
+// }
+
+// function drawCardGrid(){
+//   // push cards into the array in pairs
+// }
+
+// function shuffle(){
+//   // shuffle the cards with the array
+// }
 
 
 class Cards {
-  constructor(x,y,side){
+  constructor(x, y, side) {
     this.x = x; this.y = y;
-    this.c = c;
-    this.side = side;
-    this.value = value;
+    this.value = "7";
+    this.side = side;// 1 is the back , 0 is the front
+    if (this.side === 0) {
+      this.c = color("red");
+
+    }
+    else this.c = color("blue");
+
   }
 
 
-  display(){}
+  display() {
+    fill(this.c);
+    noStroke();
+    rect(this.x, this.y, 40, 60);
 
 
-  move(){}
+  }
 
 
-  action(){}
+
+  move() {
+    if(keyIsPressed === true){
+      if(this.c === color("red")){
+        this.c = color("blue");
+       
+      }
+
+   }
+
+
+  }
+
+
+  action() { }
 }
