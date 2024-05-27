5e8180e47463e6e5fe544f6b64f9dd87d0eb9f59
Silvercatmist <160178198+Silvercatmist@users.noreply.github.com>
2024-05-27 15:21:59 -0600

    Update sketch.js

--- capstone major project/sketch.js
+++ capstone major project/sketch.js
@@ -11,27 +11,32 @@ let col;
 // let back = color("red");
 
 let cardGrid =
-  [[]];
+  [];
 
 
-const NUM_ROWS = 3; const NUM_COLS = 3;
+const NUM_ROWS = 4; const NUM_COLS = 4;
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  card = new Cards(width / 2, height / 2, 0);
+  // card = new Cards(width / 2, height / 2, 1);
+  drawCardGrid();
 }
 
 function draw() {
   background(220);
   drawScreen();
-  card.display();
-  card.move();
+  for (let y = 0; y < NUM_ROWS; y++) {
+    for(let x = 0; x < NUM_COLS; x++){
+      cardGrid[y][x].display()
+    }
+  }
+
 }
 
 // function mouseClicked() {
- 
+
 // }
 
 
@@ -52,47 +57,64 @@ function drawScreen() {
 //   return int(constrainY / squareSize);
 // }
 
-// function drawCardGrid(){
-//   // push cards into the array in pairs
-// }
+function drawCardGrid() {
+  // push cards into the array in pairs
+  for (let y = 0; y < NUM_ROWS; y++) {
+    let cardRow = [];
+    for(let x = 0; x < NUM_COLS; x++){
+      cardRow.push(new Cards(width/2 * 0.75 + x*100, height/2 * 0.75 + y*100, 1))
+    }
+    cardGrid.push(cardRow);
+  }
+}
 
 // function shuffle(){
 //   // shuffle the cards with the array
 // }
-
+function mousePressed() {
+  for (let y = 0; y < NUM_ROWS; y++) {
+    for(let x = 0; x < NUM_COLS; x++){
+      cardGrid[y][x].mouseEvent()
+    }
+  }
+}
 
 class Cards {
   constructor(x, y, side) {
     this.x = x; this.y = y;
     this.value = "7";
-    this.side = side;// 1 is the back , 0 is the front
-    if (this.side === 0) {
-      this.c = color("red");
+    this.side = side;// 1(blue) is the back , 0(red) is the front
+    if (this.side === 1) {
+      this.c = color("blue");
 
     }
-    else this.c = color("blue");
+    else this.c = color("red");
 
   }
 
-
+  mouseEvent() {
+     if (this.side === 1) {
+        this.c = color("red");
+        this.side = 0;
+      }
+      else {
+        if (this.side === 0) {
+          this.c = color("blue");
+          this.side = 1;
+        }
+      }
+  }
   display() {
     fill(this.c);
     noStroke();
-    rect(this.x, this.y, 40, 60);
+    rect(this.x, this.y, 60, 80);
 
-
+   
   }
 
 
-
   move() {
-    if(keyIsPressed === true){
-      if(this.c === color("red")){
-        this.c = color("blue");
-       
-      }
 
-   }
 
 
   }
